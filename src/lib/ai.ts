64b26e0f67e1829789a31916003552283357a44a
MiguelHigueraDev <133175356+MiguelHigueraDev@users.noreply.github.com>
2025-06-02 14:21:15 -0400

    fix: improve prompt and add more temperature to it

--- src/lib/ai.ts
+++ src/lib/ai.ts
@@ -5,6 +5,8 @@ import { useInterviewStore } from "@/stores/interview";
 import { getDifficultyLabel, getDurationLabel } from "./utils";
 import { LiveCodingInterview, liveCodingInterviewSchema } from "./types";
 
+const TEST_CASES_COUNT = 6;
+
 export const generateLiveCodingInterview =
   async (): Promise<LiveCodingInterview> => {
     const { difficulty, duration } = useInterviewStore.getState();
@@ -14,11 +16,13 @@ export const generateLiveCodingInterview =
       model: google("gemini-2.0-flash"),
       prompt: `Generate a ${getDifficultyLabel(
         difficulty
-      )} problem with test cases for a ${getDurationLabel(
+      )} problem with ${TEST_CASES_COUNT} test cases for a ${getDurationLabel(
         duration
       )} live coding interview. Include the initial code for both TypeScript and JavaScript
       , which also logs the test cases to the console.`,
       schema: liveCodingInterviewSchema,
+      temperature: 1.7,
+      maxRetries: 3,
     });
     return response.object;
   };
